c2dbf72739ffa74d97dc15f982d2ce2111247686
Martín Mateos Sánchez <marmatsan.dev@gmail.com>
2024-02-12 12:39:46 +0100

    feat: Implementing WelcomeScreen.kt

--- onboarding/onboarding_ui/src/main/kotlin/com/marmatsan/dev/onboarding_ui/WelcomeScreen.kt
+++ onboarding/onboarding_ui/src/main/kotlin/com/marmatsan/dev/onboarding_ui/WelcomeScreen.kt
@@ -1,12 +1,43 @@
 package com.marmatsan.dev.onboarding_ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.marmatsan.dev.core_ui.dimensions.LocalSpacing
 
 @Composable
 fun WelcomeScreen(
     onCreatePlantClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val spacing = LocalSpacing.current
+
+    Column(
+        modifier = modifier
+            .background(color = MaterialTheme.colorScheme.background)
+            .padding(all = spacing.default),
+        verticalArrangement = Arrangement.spacedBy((-391).dp, Alignment.Top),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+        }
+        Column(
+            verticalArrangement = Arrangement.spacedBy(spacing.default, Alignment.Top),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+
+        }
+    }
 
 }
