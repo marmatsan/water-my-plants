1600a7ee82d251b56bdf2c5ccf4f690ae98b6969
Martín Mateos Sánchez <marmatsan.dev@gmail.com>
2024-02-15 18:07:23 +0100

    refactor: Implementing PlantScreen

--- catalog/catalog_ui/build.gradle.kts
+++ catalog/catalog_ui/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(plugins.plugins.com.android.library)
     id("com.marmatsan.android")
+    id("com.marmatsan.compose")
 }
 
 dependencies {
---
+++ catalog/catalog_ui/src/main/kotlin/com/marmatsan/dev/catalog_ui/screens/PlantScreen.kt
@@ -0,0 +1,19 @@
+package com.marmatsan.dev.catalog_ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PlantScreen(
+    modifier: Modifier = Modifier,
+    onCreatePlantClick: () -> Unit
+) {
+
+    Column(
+        modifier = modifier
+    ) {
+
+    }
+
+}
